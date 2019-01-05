b1606d02537969de200871d19cbd23bee08e6f8a
Christoph Benjamin Weber <kriswep@wetainment.com>
2019-01-05 10:14:07 +0100

    use className

--- src/App.js
+++ src/App.js
@@ -11,25 +11,25 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div
-          class="bg-teal-lightest border-t-4 border-teal rounded rounded-b text-teal-darkest px-4 py-3 shadow-md"
+          className="bg-teal-lightest border-t-4 border-teal rounded rounded-b text-teal-darkest px-4 py-3 shadow-md"
           role="alert"
         >
-          <div class="flex justify-center">
+          <div className="flex justify-center">
             <svg
-              class="h-8 w-6 text-teal mr-4"
+              className="h-8 w-6 text-teal mr-4"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
             >
               <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
             </svg>
             <div>
-              <p class="font-bold">
+              <p className="font-bold">
                 With{' '}
-                <a href="https://tailwindcss.com/" class="text-grey-darker">
+                <a href="https://tailwindcss.com/" className="text-grey-darker">
                   Tailwind CSS
                 </a>
               </p>
-              <p class="text-sm">
+              <p className="text-sm">
                 To get started, edit <code>src/App.js</code> and save to reload.
               </p>
             </div>
